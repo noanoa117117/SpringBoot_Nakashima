5a8e244b82c320deec39af49c0ab75961bb93185
noanoa117117 <sinigamiyuuna@gmail.com>
2023-02-28 21:04:02 +0900

    11.2.4

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -1,16 +1,23 @@
 package com.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	/*セキュリティの対象外を設定*/
 	@Override
 	public void configure(WebSecurity web) throws Exception{
@@ -49,15 +56,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	/*認証の設定*/
 	protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+		
+		PasswordEncoder encoder = passwordEncoder();
+		
 		//インメモリ処理
 		auth
 			.inMemoryAuthentication()
 				.withUser("user")//userを追加
-					.password("user")
+					.password(encoder.encode("user"))
 					.roles("GENERAL")
 				.and()
 				.withUser("admin")
-					.password("admin")
+					.password(encoder.encode("admin"))
 					.roles("ADMIN");
 	}
 }
