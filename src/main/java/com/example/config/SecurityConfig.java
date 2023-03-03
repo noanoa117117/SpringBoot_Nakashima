801f9d6f7aba23d22f03e0690b18f6b854addca9
noanoa117117 <sinigamiyuuna@gmail.com>
2023-03-03 22:26:20 +0900

    12.2.6

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -54,6 +55,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.usernameParameter("userId")//ログインページのパスワード
 				.passwordParameter("password")
 				.defaultSuccessUrl("/user/list",true);//成功後の遷移先
+		
+		//ログアウト処理
+		http
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login?logout");
 				
 				//CSRF対策を無効に
 				http.csrf().disable();
